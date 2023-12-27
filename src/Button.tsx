54e4b7bfd5c20e1b1603de45a19caadf36efe59f
ALbirdEX <Alex123890@gmail.com>
2023-12-28 00:59:12 +0300

    created handlers

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import classes from './Counter/Counter.module.css'
 
 type PropsTypeButton = {
+    name: string;
+    data: number;
+    maxData?: number;
+    startData?: number
     onClickHandler: () => void;
-    name: string
 }
 
-export const Button= (props: PropsTypeButton) => {
+export const Button = (props: PropsTypeButton) => {
+    let bool = props.data === props.startData || props.data === props.maxData;
     return (
-        <button className={classes.button} onClick={props.onClickHandler}>{props.name}</button>
+        <button className={classes.button}
+                onClick={props.onClickHandler}
+                disabled={bool}>
+            {props.name}
+        </button>
     );
 };
--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,27 +1,39 @@
-import React, {useState} from 'react';
-import classes from './Counter.module.css'
+import React, {ChangeEvent, useState} from 'react';
 import {Button} from "../Button";
+import classes from './Counter.module.css'
+
+let startData = 0
+let maxData = 0
 
 export const Counter: React.FC = () => {
-    const [data, setData] = useState<number>(0)
+    const [data, setData] = useState<number>(startData)
 
     const onClickIncrement = () => {
         setData(data + 1)
     }
     const onClickReset = () => {
-        setData(0)
+        setData(startData)
+    }
+
+    const startValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const numStart = Number(event.currentTarget.value)
+        startData = numStart;
+        setData(numStart)
+    }
+    const maxValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        //const numMax = Number(event.currentTarget.value)
+        maxData = Number(event.currentTarget.value);
+        //setData(startData)
     }
     return (
-        <div>
-            <div>{data}</div>
+        <div className={classes.div}>
+            <div><h1>{data}</h1></div>
             <div>
-               {/* <button className={classes.button} onClick={onClickIncrement}>Increment</button>
-                <button className={classes.button} onClick={onClickReset}>RESET</button>*/}
-                <Button onClickHandler={onClickIncrement} name={"Increment"}/>
-                <Button onClickHandler={onClickReset} name={"RESET"}/>
+                <Button onClickHandler={onClickIncrement} name={"Increment"} maxData={maxData} data={data}/>
+                <Button onClickHandler={onClickReset} name={"RESET"} startData={startData} data={data}/>
             </div>
-            <input placeholder={"START value"}></input>
-            <input placeholder={"MAX value"}></input>
+            <input placeholder={"START value"} onChange={startValueHandler}></input>
+            <input placeholder={"MAX value"} onChange={maxValueHandler}></input>
         </div>
     );
 };
