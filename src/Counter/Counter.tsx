2088cf538c20489cd26056214bff28e53f7320d1
ALbirdEX <alex123890@gmail.com>
2024-07-01 16:10:52 +0300

    added local storage used useEffect

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button} from "./Button";
 import classes from './Counter.module.css'
 import {Input} from "./Input";
@@ -8,20 +8,45 @@ const START_DATA = 0
 const MAX_DATA = 0
 
 export const Counter: React.FC = () => {
-    const [maxData, setMaxData] = useState<number>(MAX_DATA)
-    const [data, setData] = useState<number>(START_DATA)
+
+    const [maxData, setMaxData] = useState<number>(START_DATA)
+    const [data, setData] = useState<number>(MAX_DATA)
     const [collapsed, setCollapsed] = useState<boolean>(false)
+
+    useEffect(() => {
+        let valueAsString = localStorage.getItem("maxValue")
+        let valueAsString2 = localStorage.getItem("startValue")
+
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setMaxData(newValue)
+        }
+        if (valueAsString2) {
+            let newValue2 = JSON.parse(valueAsString2)
+            setData(newValue2)
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("maxValue", JSON.stringify(maxData))
+    }, [maxData])
+    useEffect(() => {
+        localStorage.setItem("startValue", JSON.stringify(data))
+    }, [data])
+
+
     const disabledButton = data >= maxData || data < 0
+
+    const outputValue = disabledButton ? "Enter correct data" : data
 
     const onClickIncrement = () => {
         setData(data + 1)
     }
     const onClickReset = () => {
         setData(START_DATA)
+        setMaxData(MAX_DATA)
     }
-
     const startValueHandler = (value: number) => {
-        debugger
         setData(value)
     }
     const maxValueHandler = (value: number) => {
@@ -32,35 +57,33 @@ export const Counter: React.FC = () => {
     }
 
     return (
-        <>
-            <div className={disabledButton ? classes.errorCount :classes.count}>
+        <div className={disabledButton ? classes.errorCount : classes.count}>
+            <div>
+                <h1>{outputValue}</h1></div>
+            <div>
+                <Button onClickHandler={onClickIncrement} name={"Increment"} maxData={maxData} data={data}
+                        startData={data} disable={disabledButton}/>
+                <Button onClickHandler={onClickReset} name={"RESET"} startData={data} data={data} maxData={maxData}
+                        disable={data === 0}/>
                 <div>
-                    <h1>{disabledButton ? "Enter correct data" : data}</h1></div>
-                <div>
-                    <Button onClickHandler={onClickIncrement} name={"Increment"} maxData={maxData} data={data}
-                            startData={data} disable={disabledButton}/>
-                    <Button onClickHandler={onClickReset} name={"RESET"} startData={data} data={data} maxData={maxData}
-                            disable={data === 0}/>
-                    <div>
-                        {collapsed ?
-                            <>
-                                <div>
-                                    MAX value:
-                                </div>
-                                <Input type={"number"} value={maxData} onChange={maxValueHandler}/>
-                                <div>
-                                    START value:
-                                </div>
-                                <Input type={"number"} value={data} onChange={startValueHandler}/>
-                                <Button name={"Apply"} data={data} maxData={data} startData={data}
-                                        collapsed={collapsed} onClickHandler={settingHandler}/>
-                            </>
-                            :
-                            <Button name={"Setup menu"} data={data} maxData={maxData} startData={data}
-                                    onClickHandler={settingHandler}/>}
-                    </div>
+                    {collapsed ?
+                        <>
+                            <div>
+                                MAX value:
+                            </div>
+                            <Input type={"number"} value={maxData} onChange={maxValueHandler}/>
+                            <div>
+                                START value:
+                            </div>
+                            <Input type={"number"} value={data} onChange={startValueHandler}/>
+                            <Button name={"Apply"} data={data} maxData={data} startData={data}
+                                    collapsed={collapsed} onClickHandler={settingHandler}/>
+                        </>
+                        :
+                        <Button name={"Setup menu"} data={data} maxData={maxData} startData={data}
+                                onClickHandler={settingHandler}/>}
                 </div>
             </div>
-        </>
+        </div>
     );
 };
